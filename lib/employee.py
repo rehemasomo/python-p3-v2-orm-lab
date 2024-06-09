2086afb93bdc324f785bb1d8865ef9f83142e3d2
rehemasomo <rehemasomo30@gmail.com>
2024-06-09 16:21:00 +0300

    done

--- lib/employee.py
+++ lib/employee.py
@@ -185,6 +185,14 @@ class Employee:
         row = CURSOR.execute(sql, (name,)).fetchone()
         return cls.instance_from_db(row) if row else None
 
-    def reviews(self):
-        """Return list of reviews associated with current employee"""
-        pass
+def reviews(self):
+    """Return list of reviews associated with current employee"""
+    from review import Review  
+    
+    sql = """
+        SELECT * FROM reviews
+        WHERE employee_id = ?
+    """
+    CURSOR.execute(sql, (self.id,))
+    rows = CURSOR.fetchall()
+    return [Review.instance_from_db(row) for row in rows]
